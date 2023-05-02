69e5b5bfd4e5e91723821d9a0932d889cd0341ab
menno.vreman@tele2.nl <menno.vreman@tele2.nl>
2023-05-02 15:52:42 +0200

    fixed button

--- src/components/Base.vue
+++ src/components/Base.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div class="bg-gray-300 h-screen p-24 w-full fixed ">
+  <div class="bg-gray-300 h-screen p-24 w-full fixed">
     <div class="bg-white-100 h-full rounded-xl border-none ">
         <slot />
     </div>
